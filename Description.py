51d0cc20265a76a5e45979310f08bcdaf4f920b2
Razor-Z-Pi <popovp.popoff2001@gmail.com>
2024-08-30 23:40:41 +0700

    fix py

--- Description.py
+++ Description.py
@@ -1,1 +1,8 @@
-print("Пончик для блендера находиться в архиви. Его нужно разархивировать!!! -//- The blender donut is in the archive. It needs to be unzipped!!!")
+import zipfile
+
+print("Пончик для блендера находиться в архиви. -//- The blender donut is in the archive.")
+
+fantasy_zip = zipfile.ZipFile('../untitled.7z') # Путь для архива 
+fantasy_zip.extractall('...') # Путь для распаковки
+
+fantasy_zip.close()
